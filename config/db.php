905da0bedea6f43c3969ea8368d8db393ae7f246
DeringSalazar <desalazarsa@est.utn.ac.cr>
2025-09-05 13:51:01 -0600

    modificación en db.php

--- config/db.php
+++ config/db.php
@@ -12,7 +12,7 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false, // Use real prepared statements
         PDO::ATTR_STRINGIFY_FETCHES => false, // Keep data types
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'"
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
     ]);
 } catch(PDOException $e) {
     // Log error securely without exposing sensitive information
